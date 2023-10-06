f8d97fe66a921a6bc224f1d5bfc5ed3f5fd0e658
Andrea Dashe <andrea.r.dashe@gmail.com>
2023-10-06 10:53:27 -0700

    Added firebase user authentication/signup

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,33 +1,29 @@
 import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup'
 
 // styles
 import styles from './Signup.module.css'
 
 export default function Signup() {
-  const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const { signup, isPending, error } = useSignup()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(username, email, password)
+    signup(email, password, displayName)
   }
 
   return (
     <form onSubmit={handleSubmit} className={styles['signup-form']}>
-      <h2>Signup</h2>
-      <label>
-        <span>username:</span>
-        <input
-          type="text"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
+      <h2>sign up</h2>
       <label>
         <span>email:</span>
         <input
           type="email"
           onChange={(e) => setEmail(e.target.value)}
+          value={email}
         />
       </label>
       <label>
@@ -35,9 +31,20 @@ export default function Signup() {
         <input
           type="password"
           onChange={(e) => setPassword(e.target.value)}
+          value={password}
         />
       </label>
-      <button className="btn">Signup</button>
+      <label>
+        <span>display name:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      { !isPending && <button className="btn">sign up</button> }
+      { isPending && <button className="btn" disabled>loading</button> }
+      { error && <p>{error}</p> }
     </form>
   )
 }
