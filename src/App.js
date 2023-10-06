fa28107eb12209917e5847d41dc41055fdbb6143
Andrea Dashe <andrea.r.dashe@gmail.com>
2023-10-06 08:47:12 -0700

    Added page components and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// page components
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
 function App() {
   return (
     <div className="App">
 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
